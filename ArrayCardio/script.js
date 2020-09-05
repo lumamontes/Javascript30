887ef01662c9156199c29c1ad77713f3d15cfb6f
Luma Montes <lumagoesmontes@gmail.com>
2020-09-04 23:49:53 -0300

    exemplo de filter e map

--- ArrayCardio/script.js
+++ ArrayCardio/script.js
@@ -1,11 +1,23 @@
 const inventors = [
     { first: 'Isaac', last: 'Newton', year: 1645, passed:1727},
     { first: 'Isaac', last: 'Newton', year: 1645, passed:1727},
-    { first: 'Isaac', last: 'Newton', year: 1645, passed:1727}
+    { first: 'Isaac', last: 'Newton', year: 1500, passed:1727},
+    { first: 'Isaac', last: 'Newton', year: 1500, passed:1727},
+    { first: 'Isaac', last: 'Newton', year: 1500, passed:1727},
+    { first: 'Isaac', last: 'Newton', year: 1510, passed:1727}
 ]
 
 const people= [
     'Beck, Glenn', 'Becker, Carl', 'Becket, Samuel'
 ]
 
-Array.prototype.filter()
+// Array.prototype.filter()
+// filter the list of invertos for those who were born in the 1500's
+const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600))
+console.table(fifteen)
+
+// Array.prototype.sort()
+//2. Give us an array of theinventory first and last names
+
+const names = inventors.map((name => (`${name.first} ${name.last}`)))
+console.log(names)
