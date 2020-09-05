2cbe3944bb3e773de87089d2ffb07dd4e7607173
Luma Montes <lumagoesmontes@gmail.com>
2020-09-04 23:30:20 -0300

    adicionado variaveis

---
+++ ArrayCardio/script.js
@@ -0,0 +1,11 @@
+const inventors = [
+    { first: 'Isaac', last: 'Newton', year: 1645, passed:1727},
+    { first: 'Isaac', last: 'Newton', year: 1645, passed:1727},
+    { first: 'Isaac', last: 'Newton', year: 1645, passed:1727}
+]
+
+const people= [
+    'Beck, Glenn', 'Becker, Carl', 'Becket, Samuel'
+]
+
+Array.prototype.filter()
